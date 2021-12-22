7f1db3b3705a87ca7794a3a4c8cb286d1fd993c6
DENISA-NASTACA MECHETIUC <denisa.mechetiuc@stud.ubbcluj.ro>
2021-12-22 16:16:06 +0200

    Laborator 11

--- Mechetiuc_Denisa_Lab10/App.xaml.cs
+++ Mechetiuc_Denisa_Lab10/App.xaml.cs
@@ -9,15 +9,15 @@ namespace Mechetiuc_Denisa_Lab10
 {
     public partial class App : Application
     {
-        static ShoppingListDatabase database;
+        static ShopListDatabase database;
 
-        public static ShoppingListDatabase Database
+        public static ShopListDatabase Database
         {
             get
             {
                 if (database == null)
                 {
-                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                    database = new ShopListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
                 }
                 return database;
             }
